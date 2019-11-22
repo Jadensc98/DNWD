b6a1216325054b001c8914a861d9bcb607208f29
Jaden Scott <jadenscott98@yahoo.com>
2019-11-22 11:02:31 -0500

    Add products scaffold

---
+++ db/migrate/20191122160211_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :size
+      t.string :fit
+      t.string :color
+      t.decimal :price
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_22_160211) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "size"
+    t.string "fit"
+    t.string "color"
+    t.decimal "price"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
