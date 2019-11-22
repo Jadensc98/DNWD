a9326c18f515e6170bd5e47145a2adf8bad84f0f
Jaden Scott <jadenscott98@yahoo.com>
2019-11-22 12:45:59 -0500

    Configure products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+    mount_uploader :image, ImageUploader
+
+    validates :name, :price, :size, :fit, :color, presence: true
+    validates :description, length: { maximum: 1000, too_long: "%{count} characters is
+    the maximum allowed."}
+    validates :name, length: { maximum: 140, too_long: "%{count} characters is the
+    maximum allowed."}
+    validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
+
+    SIZE = %w{ XS S M L XL }
 end
