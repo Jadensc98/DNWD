78f767385ab8863e7cd750ed746ab5dd3fc60655
Jaden Scott <jadenscott98@yahoo.com>
2019-11-22 13:35:04 -0500

    Add admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   resources :categories, only: [:show]
   resources :products, only: [:show, :index]
   root 'storefront#index'
+
+  namespace :admin do
+    root to: 'storefront#index'
+    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
