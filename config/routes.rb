363cafa7fd900f3f530255cfc51ea3e029c18930
Jaden Scott <jadenscott98@yahoo.com>
2019-12-05 11:43:46 -0500

    Remove admin going to add admin to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users
   resources :categories, only: [:show]
-  resources :products, only: [:show, :index]
+  resources :products, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   root 'storefront#index'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  namespace :admin do
-    root to: 'storefront#index'
-    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
-    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
-  end 
+  #namespace :admin do
+    #root to: 'storefront#index'
+    #resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    #resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  #end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
