768add04a132b7eb60fd8b239b9486d9c55d741d
Jaden Scott <jadenscott98@yahoo.com>
2019-12-05 11:53:59 -0500

    Fix product pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,7 @@ class Product < ApplicationRecord
     has_many :product_categories, dependent: :destroy
     has_many :categories, through: :product_categories
 
-    validates :name, :price, :size, :fit, :color, presence: true
+    validates :price, :size, :color, presence: true
     validates :description, length: { maximum: 1000, too_long: "%{count} characters is
     the maximum allowed."}
-    validates :name, length: { maximum: 140, too_long: "%{count} characters is the
-    maximum allowed."}
-    validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
-
-    SIZE = %w{ XS S M L XL }
 end
--- db/migrate/20191122160211_create_products.rb
+++ db/migrate/20191122160211_create_products.rb
@@ -2,9 +2,8 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :size
-      t.string :fit
       t.string :color
-      t.decimal :price, precision: 5, scale: 2, default: 0
+      t.decimal :price
       t.string :description
 
       t.timestamps
